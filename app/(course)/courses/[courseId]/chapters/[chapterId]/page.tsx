a37869cacd8ac7e6e5ec38dc223d1989c3984417
rach <rachelpytse@gmail.com>
2024-02-25 15:55:46 +0100

    Added course enroll button

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -3,6 +3,10 @@ import { Banner } from "@/components/banner";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { VideoPlayer } from "./_components/video-player";
+import { CourseEnrollButton } from "./_components/course-enroll-button";
+import { Separator } from "@/components/ui/separator";
+import { Preview } from "@/components/preview";
+import { File } from "lucide-react";
 
 const ChapterIdPage = async ({
     params
@@ -36,33 +40,75 @@ const ChapterIdPage = async ({
     const completeOnEnd = !!purchase && !userProgress?.isCompleted
 
     return (
-    <div>
-        {userProgress?.isCompleted && (
-            <Banner
-                variant="success"
-                label="You already completed this chapter"
-            />
-        )}
-        {isLocked && (
-            <Banner
-                variant="warning"
-                label="You need to purchase this course to watch this chapter"
-            />
-        )}
-        <div className="flex flex-col max-w-4xl mx-auto pb-20">
-            <div className="p-4">
-                <VideoPlayer
-                    chapterId={params.chapterId}
-                    title={chapter.title}
-                    courseId={params.courseId}
-                    nextChapterId={nextChapter?.id}
-                    playbackId={muxData?.playbackId!}
-                    isLocked={isLocked}
-                    completeOnEnd={completeOnEnd}
+        <div>
+            {userProgress?.isCompleted && (
+                <Banner
+                    variant="success"
+                    label="You already completed this chapter"
                 />
+            )}
+            {isLocked && (
+                <Banner
+                    variant="warning"
+                    label="You need to purchase this course to watch this chapter"
+                />
+            )}
+            <div className="flex flex-col max-w-4xl mx-auto pb-20">
+                <div className="p-4">
+                    <VideoPlayer
+                        chapterId={params.chapterId}
+                        title={chapter.title}
+                        courseId={params.courseId}
+                        nextChapterId={nextChapter?.id}
+                        playbackId={muxData?.playbackId!}
+                        isLocked={isLocked}
+                        completeOnEnd={completeOnEnd}
+                    />
+                </div>
+                <div>
+                    <div className="p-4 flex flex-col md:flex-row items-center justify-between">
+                        <h2 className="text-2xl font-semibold mb-2">
+                            {chapter.title}
+                        </h2>
+                        {purchase ? (
+                            <div>
+                                {/* //TODO: Add CourseProgressButton */}
+                            </div>
+                        ) : (
+                            <CourseEnrollButton
+                                courseId={params.courseId}
+                                price={course.price!}
+                            />
+                        )}
+                    </div>
+                    <Separator/>
+                    <div>
+                        <Preview value={chapter.description!}/>
+                    </div>
+                    {!!attachments.length && (
+                        <>
+                            <Separator/>
+                            <div className="p-4">
+                                {attachments.map((attachment) => (
+                                    <a
+                                        href={attachment.url}
+                                        target="_blank"
+                                        key={attachment.id}
+                                        className="flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:underline"
+                                    >
+                                        <File/>
+                                        <p className="line-clamp-1">
+                                            {attachment.name}
+                                        </p>
+                                    </a>
+                                ))}
+                            </div>
+                        </>
+                    )}
+                </div>
             </div>
         </div>
-    </div> );
+    );
 }
  
 export default ChapterIdPage;
