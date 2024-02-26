3a5b6f1e80dcc18a7dd2ad8a125e35b774fc39c4
rach <rachelpytse@gmail.com>
2024-02-26 12:24:30 +0100

    Added protection frontend and backend

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -3,6 +3,7 @@ import { auth } from "@clerk/nextjs"
 import { NextResponse } from "next/server"
 
 import { db } from "@/lib/db";
+import { isTeacher } from "@/lib/teacher";
 
 const mux  = new Mux({
     tokenId: process.env['MUX_TOKEN_ID'],
@@ -16,7 +17,7 @@ export async function DELETE(
     try {
         const {userId} = auth()
 
-        if(!userId) {
+        if(!userId || !isTeacher(userId)) {
             return new NextResponse("Unauthorized", {status: 401})
         }
 
@@ -67,7 +68,7 @@ export async function PATCH(
         const values = await req.json()
         console.log(values)
 
-        if(!userId) {
+        if(!userId || !isTeacher(userId)) {
             return new NextResponse("Unauthorized", { status: 401 })
         }
 
