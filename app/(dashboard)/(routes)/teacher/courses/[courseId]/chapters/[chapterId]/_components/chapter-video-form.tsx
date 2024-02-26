a0b70ca9c1fd7273396cb2c44fb38735dc14c1ed
rach <rachelpytse@gmail.com>
2024-02-26 12:34:48 +0100

    Changed the apostrophe mark

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
@@ -91,7 +91,7 @@ export const ChapterVideoForm = ({
             }}
           />
           <div className="text-xs text-muted-foreground mt-4">
-            Upload this chapter's video
+            Upload this chapter&apos;s video
           </div>
         </div>
       )}
